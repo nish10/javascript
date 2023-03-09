281f250f6463ee9c63684576b61c1c8af2bcaf58
Kumar Nishant <nishant.3.kumar@nokia.com>
2023-03-10 00:39:52 +0530

    basic javascript intro

---
+++ sandbox/app.js
@@ -0,0 +1,223 @@
+// Log to console
+
+/*
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error("This is some error");
+console.clear();
+console.warn('This is a warning');
+console.time('Hello');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+console.timeEnd('Hello');
+
+*/
+
+/*
+    Multi line comments
+*/
+
+// var, let, const
+
+// var name = 'Nish';
+// console.log(name);
+// name = 'Nishant';
+// console.log(name);
+
+// // Init Var
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
+
+// // letters, numbers, _, $
+// // cannot start with number
+
+// // multiword vars
+
+// var firstName = 'John'; // Camel Case
+// var first_name = 'Sara'; // underscore method
+// var FirstName = 'Tom'; // Pascal Case
+// var firstname; // not recommended
+
+// LET
+// let name;
+// name = 'Nish';
+// console.log(name);
+// name = 'Nishant';
+// console.log(name);
+
+// CONST
+
+// const name = 'John';
+// console.log(name);
+
+// cannot reassign
+// name = 'Sara'
+// have to assign a value
+
+// const greeting;
+
+// const person = {
+//     name: 'John',
+//     age: 30,
+// }
+
+// person.name = 'Sara';
+// person.age = 25;
+// console.log(person);
+
+// const numbers = [1,2,3,4,5];
+// numbers.push(6);
+// console.log(numbers);
+
+// PRMIMITE DATA TYPES
+
+// // strimg
+// const name = 'Nishant';
+
+// console.log(typeof name);
+
+// // number
+// const age = 28;
+
+// console.log(typeof age);
+
+// //Null
+
+// const car = null;
+// console.log(typeof car);
+
+// // undefined
+
+// let und;
+// console.log(typeof und);
+
+// // symbol
+
+// const sym = Symbol();
+// console.log(typeof sym);
+
+// // REFERENCE DATA TYPES - Objects
+
+// // Array
+// const hobbies = ['movies', 'music', 1];
+// console.log(typeof hobbies);
+
+// // object literal
+
+// const address = {
+//     city: 'Boston',
+//     state: 'MA',
+// }
+
+// console.log(typeof address);
+
+// const today = new Date();
+// console.log(today);
+// console.log(typeof today);
+
+
+// type conversion
+
+// let val;
+
+
+// // number to string
+
+// val = String(5555);
+// val = String(4+4);
+
+// //bool to string
+// val = String(true);
+
+// // date to string
+// val = String(new Date());
+
+// // array to string
+
+// val = String([1,2,3,4])
+
+// // toString()
+
+// val = (5).toString();
+// val = (true).toString();
+
+// // String to number
+
+// val = Number('5');
+// val = Number(true);
+// val = Number(false);
+// val = Number(null);
+// val = Number('Hello');
+// val = Number([1,2,3]);
+
+// val = parseInt('100.20');
+// val = parseFloat('100.20');
+
+
+// // output
+
+// console.log(val);
+// console.log(typeof val);
+// // console.log(val.length);
+// console.log(val.toFixed(2));
+
+// const val1 = String(5);
+// const val2 = 6;
+
+// const sum = Number(val1 + val2);
+
+// console.log(sum);
+// console.log(typeof sum);
+
+
+// const num1 = 100;
+// const num2 = 50;
+
+// let val;
+// val = num1 + num2;
+// val = num1 + num2;
+// val = num1 + num2;
+// val = num1 / num2;
+// val = num1 % num2;
+
+// // Math Object
+
+// val = Math.PI;
+// val = Math.E;
+// val = Math.round(2.4);
+// val = Math.ceil(2.4);
+// val = Math.floor(2.8);
+// val = Math.sqrt(64);
+// val = Math.abs(-3);
+// val = Math.pow(8, 2);
+// val = Math.min(8, 3, 4, 2);
+// val = Math.max(8, 3, 4, 2);
+// val = Math.floor(Math.random() * 20 + 1);
+
+// console.log(val);
+
+
+
+
+
+
+ 
+
+
+
+
+
